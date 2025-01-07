03c34502948dd49cf4ef426083529c1dd0a40cef
CourierLFF <45730525+CourierLFF@users.noreply.github.com>
2025-01-07 10:05:05 -0500

    Add LeafNode

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -16,3 +16,21 @@ class HTMLNode():
     
     def __repr__(self):
         return f"TAG: {self.tag} VALUE: {self.value} CHILDREN: {self.children} PROPS: {self.props_to_html()}"
+    
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError
+        elif self.tag == None:
+            return str(self.value)
+        else:
+            if self.props == None:
+                return f"<{self.tag}>{self.value}</{self.tag}>"
+            else:
+                return f"<{self.tag} {super().props_to_html()}>{self.value}</{self.tag}>"
+        
+        
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_one(self):
@@ -10,6 +10,22 @@ class TestHTMLNode(unittest.TestCase):
         }
         new_node = HTMLNode("h1", "Header One", ["Child One", "Child Two"], props)
         self.assertTrue(new_node)
+    
+    def test_two(self):
+        new_leafNode = LeafNode("p", "I am a paragraph!")
+        self.assertTrue(new_leafNode.to_html())
+    
+    def test_two(self):
+        new_leafNode = LeafNode("a", "I am a link!", {"href" : "https://www.google.com"})
+        self.assertTrue(new_leafNode.to_html())
+    
+    def test_three(self):
+        new_leafNode = LeafNode("p", None)
+        self.assertRaises(ValueError)
+    
+    def test_four(self):
+        new_leafNode = LeafNode(None, "I have no tag!")
+        self.assertTrue(new_leafNode.to_html())
 
 if __name__ == "__main__":
     unittest.main()
