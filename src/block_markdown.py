cd799b87a6dc95e852b2029ade9cbf1a14e70497
CourierLFF <45730525+CourierLFF@users.noreply.github.com>
2025-01-12 09:49:09 -0500

    Fix markdown_to_blocks to split the proper blocks

--- src/block_markdown.py
+++ src/block_markdown.py
@@ -1,6 +1,19 @@
 def markdown_to_blocks(markdown):
-    block_list = markdown.split("\n")
+    block_list = markdown.split("\n\n")
     block_list = list(filter(lambda x: x.strip(), block_list))
     for x in range(len(block_list)):
         block_list[x] = block_list[x].strip(" ")
-    print(block_list)
+    return block_list
+
+
+test_string = """
+# This is a heading
+
+This is a paragraph of text. It has some **bold** and *italic* words inside of it.
+
+* This is the first list item in a list block
+* This is a list item
+* This is another list item
+"""
+
+#print(markdown_to_blocks(test_string))
