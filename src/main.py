7d310c3167b5d4d089766007a0839bee2a49793f
CourierLFF <45730525+CourierLFF@users.noreply.github.com>
2025-01-13 08:47:24 -0500

    Copy file function

--- src/main.py
+++ src/main.py
@@ -1,9 +1,38 @@
-from textnode import TextNode, TextType
+import os, shutil
+
+public_folder = "public"
+static_folder = "static"
+
+def clean_public():
+    if os.path.exists(public_folder):
+        for filename in os.listdir(public_folder):
+            file_path = os.path.join(public_folder, filename)
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)    
+
+def static_to_public_copy(working_folder, searching_folder):
+    if os.path.exists(searching_folder):
+        for filename in os.listdir(searching_folder):
+            file_path = os.path.join(searching_folder, filename)
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                shutil.copy(file_path, working_folder)
+            elif os.path.isdir(file_path):
+                os.mkdir(f"{working_folder}/{filename}")
+                new_working_folder = f"{working_folder}/{filename}"
+                new_searching_folder = f"{searching_folder}/{filename}"
+                static_to_public_copy(new_working_folder, new_searching_folder)
+
+
+    
 
 
 def main():
-    node = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-    print(node)
+    clean_public()
+    static_to_public_copy(public_folder, static_folder)
 
 
 main()
+
+
